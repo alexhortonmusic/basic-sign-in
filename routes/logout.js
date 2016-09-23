c5fe985e8ce2249ab5c2a239eebc9657dae8a391
Alex Horton <alexhortonmusic@gmail.com>
2016-09-23 15:25:21 -0500

    refactored routes/added logout

---
+++ routes/logout.js
@@ -0,0 +1,12 @@
+// 'use strict';
+//
+// const { Router } = require('express')
+// const { index, destroy } = require('../ctrl/logout')
+//
+// const router = Router()
+//
+// router.get('/logout', index)
+//
+// router.post('/logout', destroy)
+//
+// module.exports = router
